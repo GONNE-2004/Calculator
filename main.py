eb2b23e63facb66b92b1f64eab1ef011dd32bcf0
GONNE <gonne411@gmail.com>
2025-07-31 02:14:35 +0200

    remove recursion which was calling function within function and used while loop, added an exit option

--- main.py
+++ main.py
@@ -28,26 +28,31 @@ operations = {
 
 
 def calculator():
-    print(art.logo)
-    should_accumulate = True
-    num1 = float(input("What is the first number?: "))
+    while True: #main loop which keeps the program in repeat
+        print(art.logo)
+        should_accumulate = True
+        num1 = float(input("What is the first number?: "))
 
-    while should_accumulate:
-        for symbol in operations:
-            print(symbol)
-        operation_symbol = input("Pick an operation: ")
-        num2 = float(input("What is the next number?: "))
-        answer = operations[operation_symbol](num1, num2)
-        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        while should_accumulate:
+            for symbol in operations:
+                print(symbol)
+            operation_symbol = input("Pick an operation: ")
+            num2 = float(input("What is the next number?: "))
+            answer = operations[operation_symbol](num1, num2)
+            print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+            choice = input(f"\nType 'y' to continue calculating with:{answer} ,type 'n' to start a new calculation or 'Q' to exit program. ").lower()
 
-        if choice == "y":
-            num1 = answer
-        else:
-            should_accumulate = False
-            print("\n" * 20)
-            calculator()
-
+            if choice == "y":
+                num1 = answer
+            elif choice == "q":
+                print("GOODBYE!")
+                return
+            else:
+                should_accumulate = False
+                print("\n" * 20)
+                break # this break takes the progeam back to the first while loop
+                       #earlier i had called the function within the function but now i changed to using loops
+                       # the problem with recursion it isn't memory efficient
 
 calculator()
