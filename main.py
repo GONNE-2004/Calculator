177d5523fd73ce5101e74eb60f3e8aec85f7d853
GONNE <gonne411@gmail.com>
2025-08-01 05:57:25 +0200

    add button for history, add a function which formates inputs to their initial self, add function which ensures the user enters the correct data-type, added function which ensures the program won't crush if the user picked an incorrect operation symbol, added try-except in division function to avoid division by 0 and lastly formated the operation symbol function into float type in order to avoid future typerError

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import art
 
 
-def add(n1, n2):
-    return n1 + n2
+def add(n1 : float, n2 : float): # i used float becuase when dividing it always returns it plus to avoid future typeError
+    return n1 + n2              # i used something called mypy, installed via pip which shows data-types of functions or if a function is untyped
 
 
-def subtract(n1, n2):
+def subtract(n1 : float, n2 : float):
     return n1 - n2
 
 
-def multiply(n1, n2):
+def multiply(n1 : float, n2 : float):
     return n1 * n2
 
 
-def divide(n1, n2):
-    return n1 / n2
-
+def divide(n1: float, n2: float):
+    try:
+        return n1/n2
+    except ZeroDivisionError:
+        print("Error: Division by zero!")
+        return float ("non")
 
 operations = {
     "+": add,
@@ -25,26 +28,57 @@ operations = {
 }
 
 # print(operations["*"](4, 8))
+def formate_number(num : float):
+    """returns integer if number is whole else float"""
+    return int(num) if num.is_integer() else num
+
+def get_number(prompt : str):
+    """Ensure the user enter the correct data type"""
+    try:
+        return float(input(prompt))
+    except ValueError:
+        print("Please enter a valid numer!")
+
+def get_operation():
+    """prevents the  program from crashing incase the user entered an invalid operation"""
+    while True:
+        print("Available operation:")
+        for op in operations:
+            print(op)
+            if op in operations:
+                return op
+            print(f"\n{op} not in the avaliable operations")
+
 
 
 def calculator():
+    """holds the main logic"""
+    history = []
     while True: #main loop which keeps the program in repeat
         print(art.logo)
         should_accumulate = True
-        num1 = float(input("What is the first number?: "))
+        num1 =get_number("What is the first number?: ") #asks the user for input but i used function with input to ask the user here, the logic is explained using doc str
 
         while should_accumulate:
             for symbol in operations:
                 print(symbol)
-            operation_symbol = input("Pick an operation: ")
-            num2 = float(input("What is the next number?: "))
-            answer = operations[operation_symbol](num1, num2)
-            print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-            choice = input(f"\nType 'y' to continue calculating with:{answer} ,type 'n' to start a new calculation or 'Q' to exit program. ").lower()
+            operation_symbol = get_operation("Pick an operation: ")
+            num2 =get_number("What is the next number?: ") #asking for the 2nd input, used function with inputs instead of just input function
+            answer = formate_number(operations[operation_symbol](num1, num2)) # initiated a new variable then assigned the dic name plus the key name in order to use the value which's an int
+            cal_formate = f"{formate_number(num1)} {operation_symbol} {formate_number(num2)} = {formate_number(answer)} "
+            #above i tried to check if the initial number were float or integer in order to return the final input as first entered
+            print(cal_formate)
+
+            history.append(cal_formate)#here i used append instead of (+=) because it'll the ans as singlw entity into the history list
+            #whereas if i used (+=) it would have unpacked the ans as separate entity!!
+            choice = input(f"\nType 'y' to continue with:{answer},type 'n' to start a new calculation,'Q' to exit program &'h' to review history. ").lower()
 
             if choice == "y":
                 num1 = answer
+            elif choice == "h":
+                for item in history:
+                    print(item)
             elif choice == "q":
                 print("GOODBYE!")
                 return
